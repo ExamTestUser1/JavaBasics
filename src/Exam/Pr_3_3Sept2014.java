9a6b8af8c0721438afbf3d759a8834bf852a503f
desi4ok <d.andreeva@yahoo.com>
2015-01-20 23:02:08 +0200

    20.01/20h

--- src/Exam/Pr_3_3Sept2014.java
+++ src/Exam/Pr_3_3Sept2014.java
@@ -9,9 +9,7 @@ public class Pr_3_3Sept2014 {
         String[] inputArray = input.split("\\D+");
         //int[] nums = new int[inputArray.length];
 
-
-
-        for (int i = 0; i < inputArray.length; i++) {
+        for (int i = 1; i < inputArray.length; i++) {
             //nums[i] = Integer.parseInt(inputArray[i]);
             System.out.println(inputArray[i]);
         }
